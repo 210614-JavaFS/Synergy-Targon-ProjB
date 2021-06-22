2c56b29a2f18b806a1076e5f7574a91f6c7b8967
tjwoods03 <thomas.woods@revature.net>
2021-06-22 08:28:09 -0500

    added to kitchen and microwave

--- HomeTour/src/fixtures/objects/Microwave.java
+++ HomeTour/src/fixtures/objects/Microwave.java
@@ -1,5 +1,19 @@
 package fixtures.objects;
 
-public class Microwave {
+import fixtures.Fixture;
+
+public class Microwave extends Fixture implements Interactive{
+	
+	public Microwave() {
+		super();
+		this.name = "Microwave";
+		this.shortDescription = "A rusty worn out microwave on a beautiful marble island";
+		this.longDescription = "The rusted microwave looks as though it hasn't been used in over half a decade. It" + " is currently turned off";
+	}
+	
+	public void interactWith() {
+		System.out.println("You put your ramen in the microwave and try to cook it for 2 minutes");
+		this.longDescription =  "The rusted microwave looks as though it hasn't been used in over half a decade. It" + "is still turned off but has your ramen in it... cold.";
+	}
 
 }
