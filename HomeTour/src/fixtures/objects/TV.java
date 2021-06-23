c96893af5f1094597c92a835150507e607269de3
Clinton N. Obi <clintonn.obi@macusers-MacBook-Pro.local>
2021-06-23 11:24:28 -0400

    added TV and LoungeRoom

---
+++ HomeTour/src/fixtures/objects/TV.java
@@ -0,0 +1,16 @@
+package fixtures.objects;
+
+import fixtures.Fixture;
+
+public class TV extends Fixture implements Interactive {
+	public TV() {
+		super("TV",
+				"",
+				"");
+	}
+	
+	
+	public void interactWith() {
+		
+	}
+}
