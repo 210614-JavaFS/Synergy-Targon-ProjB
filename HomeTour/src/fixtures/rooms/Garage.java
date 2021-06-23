368042238fda931a26560ad7c46a627289f2f36a
lou-est <liwensky.estimar@gmail.com>
2021-06-23 15:48:22 -0400

    committing changes to my branch

--- HomeTour/src/fixtures/rooms/Garage.java
+++ HomeTour/src/fixtures/rooms/Garage.java
@@ -1,12 +1,16 @@
 package fixtures.rooms;
 
+import fixtures.objects.Lawnmower;
+
 public class Garage extends Room {
+
+	//Lawnmower lawnmower = new Lawnmower();
 	
 	public Garage() {
 		super("Garage",
 				"A cluttered garage",
 				"The storage location for the houses cars and many of its tools.",
-				+ "The foyer is to the east where the entry to the house can be seen."
+				+ "The foyer is to the east where you see the entry to the house can be seen."
 				+ "You push your way through a pile of rusty tools, brushing past used tires, where a rake sits at the top of the staircase leading to the basement.");
 
 	
