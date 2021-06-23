5f41a17b4dd3a81b0652f97d61e3b6763d455257
Hong Wu <hwu5542@gmail.com>
2021-06-23 16:01:43 -0500

    cathouse v8-2

--- HomeTour/src/fixtures/objects/CatTree.java
+++ HomeTour/src/fixtures/objects/CatTree.java
@@ -8,7 +8,7 @@ public class CatTree extends Fixture implements Interactive {
 				"A six foot tall cat tree sits in the corner.", 
 				"Perch, lookout tower, dangling toys, you spent a forturne to please your cute masters. \n"
 				+ "The shadowy little private apartments are always full of surprises. "
-				+ "Sometimes a gentle licking, sometimes a playfull bite.");
+				+ "Sometimes are gentle sniffing and licking, sometimes a playfull bite, but if its a scratch, they mean business.");
 	}
 	
 	public void interactWith () {
