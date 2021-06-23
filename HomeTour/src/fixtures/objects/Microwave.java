83ac740173bd6d617eadf27b7456d7ffc966757a
tjwoods03 <thomas.woods@revature.net>
2021-06-23 08:33:35 -0500

    added some small details

--- HomeTour/src/fixtures/objects/Microwave.java
+++ HomeTour/src/fixtures/objects/Microwave.java
@@ -12,8 +12,8 @@ public class Microwave extends Fixture implements Interactive{
 	}
 	
 	public void interactWith() {
-		//System.out.println("You put your ramen in the microwave and try to cook it for 2 minutes");
-		//this.longDescription =  "The rusted microwave looks as though it hasn't been used in over half a decade. It" + "is still turned off but has your ramen in it... cold.";
+		System.out.println("You put your ramen in the microwave and try to cook it for 2 minutes");
+		this.SetLongDescription("The rusted microwave looks as though it hasn't been used in over half a decade. It" + "is still turned off but has your ramen in it... cold.");
 	}
 
 }
--- HomeTour/src/fixtures/rooms/Garage.java
+++ HomeTour/src/fixtures/rooms/Garage.java
@@ -5,7 +5,7 @@ public class Garage extends Room {
 	public Garage() {
 		super("Garage",
 				"A cluttered garage",
-				"The storage location for the houses cars and many of its tools.",
+				"The storage location for the houses cars and many of its tools."
 				+ "The foyer is to the east where the entry to the house can be seen."
 				+ "You push your way through a pile of rusty tools, brushing past used tires, where a rake sits at the top of the staircase leading to the basement.");
 
