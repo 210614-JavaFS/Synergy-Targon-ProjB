4107d7cf89c210987c1097aae288f78416511aeb
Hong Wu <hwu5542@gmail.com>
2021-06-23 14:05:08 -0500

    cathouse-v7-4

--- HomeTour/src/game/RoomManager.java
+++ HomeTour/src/game/RoomManager.java
@@ -9,26 +9,40 @@ import fixtures.rooms.Room;
  should register their room in here.
 ************************/
 public class RoomManager {
+
+
+	// it looks like Room[] rooms is an array that stores Room objects
 	private static Room[] rooms;
 	private static Room initialRoom;
-	
+
+	public RoomManager (int numberOfRooms) {
+		RoomManager.rooms = new Room[numberOfRooms];
+	}
+
 	public static Room getInitialRoom() {
 		return initialRoom;
 	}
-	
+
+	public static void init() {			
 	public static void init(int numberOfRooms) {
 		//create slot for rooms
 		RoomManager.rooms = new Room[numberOfRooms];
 
 		//creating the house, put your rooms instantiation here
 		Basement basement = new Basement();	
-		
-		
+		Foyer foyer = new Foyer();
+		Kitchen kitchen = new Kitchen();
+
+
 		//put rooms into house, assign your rooms to an index in here
+		rooms[1] = foyer;
+		rooms[2] = basement;
+		rooms[3] = kitchen;
 		rooms[0] = basement;
 
-		
+
 		//starting room, should be be change to foyer once done constructing
+		RoomManager.initialRoom = foyer;
 		RoomManager.initialRoom = basement;
 	}
 }
