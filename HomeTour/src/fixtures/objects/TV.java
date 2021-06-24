fa6850095b2e64dc133c6b111cc2955266913e92
Clinton N. Obi <clintonn.obi@macusers-MacBook-Pro.local>
2021-06-24 08:47:00 -0400

    added to lounge & TV

--- HomeTour/src/fixtures/objects/TV.java
+++ HomeTour/src/fixtures/objects/TV.java
@@ -7,14 +7,14 @@ public class TV extends Fixture implements Interactive {
 		super("TV",
 				"An outdated faded bronze colored TV with two protruding antennas.",
 				"This TV was designed by '*** inc' in the late 1960s. The screen is gray and cracked. "
-				+"The Tv is turned on despite the crack and is emitting static noise");
+				+"The TV is turned on despite the crack and is emitting static noise");
 	}
 	
 	
 	public void interactWith() {
 		System.out.println("You hit the TV with a heavy tap turnning it off.");
 		this.SetLongDescription("The TV is warm to the touch. May need some repairs as it"
-				+ "seems to be on its last legs.");
+				+ " seems to be on its last legs.");
 	}
 }
 
--- HomeTour/src/fixtures/rooms/LoungeRoom.java
+++ HomeTour/src/fixtures/rooms/LoungeRoom.java
@@ -5,19 +5,9 @@ import fixtures.objects.TV;
 
 public class LoungeRoom extends Room {
 	
-	private Interactive loungeFeature = new TV();
+	TV tv = new TV();
 	
-	public LoungeRoom() {
-		super("LoungeRoom",
-			"A cozy-looking, but unfinished living room.",
-			"A chalky smell enters your nostrils as you walk across the heated marble floors into the room. " 
-			    + "\n"
-		   +"Overhead you can see an empty emerald chandelier swaying rhythmically. Running your hands across the rough paint job on the walls, you spot a TV in the far back "
-		   + "next to a kitchen to the east. Opposite to the kitchen you spot stairs going down to the basement."    
-		   +"\n"
-			+ "You let out a sigh as you crouch down next to the translucent window being showered with snowdrops.");
-	}
-	//this.exits[?] = RoomManager.getRoom(“kitchen or basement depending on exit number”)??;
+	private Interactive loungeFeature = new TV();
 	
 	public Interactive getLoungeFeature() {
 		return loungeFeature;
@@ -27,5 +17,25 @@ public class LoungeRoom extends Room {
 		this.loungeFeature = loungeFeature;
 	}
 
-	TV tv = new TV();
+
+	public LoungeRoom() {
+		super("LoungeRoom",
+			"A cozy-looking, but unfinished living room.",
+			"A chalky smell enters your nostrils as you walk across the heated marble floors into the room. " 
+			    + "\n"
+		   +"Overhead you can see an empty emerald chandelier swaying rhythmically."
+		   +"\n"
+		   + "Running your hands across the rough paint job on the walls, you spot a TV in the far back "
+		   + "next to a kitchen to the east. "
+		   +"\n"
+		   + "Opposite to the kitchen you spot stairs going down to the basement."    
+		   +"\n"
+			+ "You let out a sigh as you crouch down next to the translucent window being showered with snowdrops.");
+
+			//this.exits[6] = RoomManager.getRoom(“kitchen”)??;
+			//this.exits[4] = RoomManager.getRoom(“basement”)??;
+	}
+
 }
+
+	
