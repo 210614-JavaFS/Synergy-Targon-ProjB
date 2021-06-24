2c6261926cc65bc8a696c3fdec204a8c2c742b27
Hong Wu <hwu5542@gmail.com>
2021-06-24 15:13:43 -0500

    cathouse v10-catworking

--- HomeTour/src/fixtures/rooms/Outside.java
+++ HomeTour/src/fixtures/rooms/Outside.java
@@ -3,7 +3,7 @@ package fixtures.rooms;
 public class Outside extends Room {
 
 	public Outside() {
-		super("Outside",
+		super("outside",
 				"exit the house through the front gate",
 				"this door way has the magic to pop you out of the house.");
 	}
